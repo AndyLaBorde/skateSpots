c0da3966322bd69ed66fd5dc403abec06310f180
codyw9524 <codyw9524@gmail.com>
2016-04-29 15:18:08 -0500

    removed facebook login link

--- application/views/partials/login_html.php
+++ application/views/partials/login_html.php
@@ -11,18 +11,6 @@
 						<input type="password" id="password" name="password" class="form-control">
 						<button class="btn btn-default btn-sm">Log In</button>
 						<h6>through skateSpots or</h6>
-						<?php 
-							require_once 'vendor/autoload.php';
-							$fb = new Facebook\Facebook ([
-								'app_id' => '262275047452139',
-								'app_secret' => 'c26ad5790b9ee6a601e990fa9ff8c13a',
-								'default_graph_version' => 'v2.5',
-								]);
-							$helper = $fb->getRedirectLoginHelper();
-							$permissions = ['email', 'user_likes'];
-							$loginUrl = $helper->getLoginUrl('http://localhost8888/Sessions/face', $permissions);
-							echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
-						?>
 					</div>
 				</form>
 				<form id="register_link" action="/Users/index_html" method="post">
